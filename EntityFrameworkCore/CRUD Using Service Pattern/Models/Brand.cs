3b78abe9fbd25b658da5b672dc7ae1f51c8489a4
jafare2010@gmail.com <jafare2010@gmail.com>
2022-08-13 15:45:24 +0300

    CRUD using Service Pattern

---
+++ EntityFrameworkCore/CRUD Using Service Pattern/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication14_2021A.Models
+{
+    public class Brand
+    {
+        [Key]
+        public long BrandId { get; set; }
+        public string BrandName { get; set; }
+    }
+}
